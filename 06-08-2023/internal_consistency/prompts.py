ae72c0312d9dc6d4cac506bc8930fe4f81c3079e
hwelsters <corevalue.noel@gmail.com>
2023-06-03 19:54:48 -0700

    feat:collected responses with corrected grammar

--- 06-08-2023/internal_consistency/prompts.py
+++ 06-08-2023/internal_consistency/prompts.py
@@ -1,7 +1,7 @@
 BASIC_PROMPT = '''Solve the math question below:
 Solve by breaking the problem into steps. 
 First, produce the algebraic system of equations needed to solve it. 
-Second, output the final answer; format it like this: 'the answer is <put your answer here>'.
+Second, output the final answer; format it like this: 'The answer is <put your answer here>'.
 
 Question: {question}
 '''.strip() + '\n\n\n'
@@ -9,8 +9,8 @@ Question: {question}
 EXTRACT_EQUATIONS = '''
 Extract the equations from this response.
 You should give me the minimal system of equations needed to solve the question.
-You should not describe your steps, explain or label your response.
-You should separate each equation with a newline.
+You should not describe your steps, or explain or label your response.
+You should separate each equation with a new line.
 You should remove all units from your response.
 
 Response: {response}
@@ -19,7 +19,7 @@ Response: {response}
 SOLVE_SYSTEM = '''
 Solve the following system of equations:
 First, solve the equations step-by-step
-Second, output the final answer; format it like this: 'the answer is <put your answer here>'.
+Second, output the final answer; format it like this: 'The answer is <put your answer here>'.
 
 Equations:
 {equations}
