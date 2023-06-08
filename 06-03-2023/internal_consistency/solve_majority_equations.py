8f65e86e83515bab7a7a091c47e3be30b11fede9
EzraLeeASU <126364499+Wermutton@users.noreply.github.com>
2023-06-08 00:22:50 -0700

    feat: temp

--- 06-03-2023/internal_consistency/solve_majority_equations.py
+++ 06-03-2023/internal_consistency/solve_majority_equations.py
@@ -1,5 +1,5 @@
 import os
-import pandas
+import pandas as pd
 
 from sympy_solver import SympySolver
 
@@ -9,11 +9,26 @@ def flag(solved):
     if len(solved) == 0: return 'SUS'
     return 'OK'
 
+# Load response data
+sample_response = pd.read_json('../data/output/response/sample_0.jsonl', lines=True)
+
+# Function to get all the assistant's responses
+def get_responses(question_id):
+    response_row = sample_response[sample_response['question_id'] == question_id]
+    if response_row.empty:
+        return []
+    else:
+        return [choice['message']['content'] for choice in response_row.iloc[0]['choices']]
+
 for filename in os.listdir('../data/output/majority'):
     print(filename)
-    sample = pandas.read_json(f'../data/output/majority/{filename}', lines=True)
-    samplex = pandas.DataFrame()
-    samplex['solved'] = sample.apply(lambda row : SympySolver.solve_equations(row['majority_equation'].split('\n')), axis=1)
-    samplex['flag'] = samplex['solved'].apply(lambda row : flag(row))
-    samplex['solved'] = samplex['solved'].apply(lambda row : str(row))
-    samplex.to_json(f'../data/output/solve_majority/{filename}', lines=True, orient='records')
+    sample = pd.read_json(f'../data/output/majority/{filename}', lines=True)
+
+    for i in range(10):  # limit to first 10 responses
+        samplex = sample.copy()
+        samplex['solved'] = sample.apply(lambda row : SympySolver.solve_equations(row['majority_equation'].split('\n')), axis=1)
+        samplex['flag'] = samplex['solved'].apply(lambda row : flag(row))
+        samplex['solved'] = samplex['solved'].apply(lambda row : str(row))
+        
+        samplex['response'] = samplex['question_id'].apply(lambda row : get_responses(row)[i] if len(get_responses(row)) > i else None)
+        samplex.to_json(f'../data/output/solve_majority_test/sample_{i}.jsonl', lines=True, orient='records')
